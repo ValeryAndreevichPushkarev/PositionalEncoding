106a596b04caadcad335b9b0c0f67fdb55967646
ValeryAndreevichPushkarev <73181724+ValeryAndreevichPushkarev@users.noreply.github.com>
2020-10-30 16:38:49 +0700

    Update positional_encoding_7func.py

--- positional_encoding_7func.py
+++ positional_encoding_7func.py
@@ -5,7 +5,7 @@ import math
 op_names = ["add","sub","div","mul","pow","act_func","act_func_2_1"]
 #input registers
 width = 2;
-#maximum used commands
+#maximum used commands in module (set to one for implement one function)
 op_count = 2;
 maxnum=width;
 output_width = 2*width;
